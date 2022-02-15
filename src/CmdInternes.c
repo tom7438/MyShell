de4f953f1f69c6538054fd6cce9c9246dd356ffd
tom7438 <tom192010@live.fr>
2022-02-15 15:41:21 +0100

    Fichiers qui gèrent les commandes internes

--- src/CmdInternes.c
+++ src/CmdInternes.c
@@ -2,10 +2,10 @@
 #include <string.h>
 #include "CmdInternes.h"
 
-char CommandesInternes[5][10]={"pwd", "cd", "set", "echo", "type"};
+char CommandesInternes[5][10]={"pwd", "cd", "set", "echo"};
 
 int searchCmd(char *cmd){
-    for(int i=0; i<5; i++){
+    for(int i=0; i<4; i++){
         if (!strcmp(cmd, CommandesInternes[i])) {
             return 0;
         }
