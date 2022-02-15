da833f32dbb2e32ab16a6595c8e2af678ea03907
tom7438 <tom192010@live.fr>
2022-02-15 14:32:04 +0100

    Redirection d'entrées sorties ok !

--- src/shell.c
+++ src/shell.c
@@ -34,12 +34,32 @@ int main(){
 			exit(0);
 		}
 		
-		// 3) Commande simple
+		// 3) && 4) Commande simple && Redirections d'entrées et sorties
+
 		pid_t pid=Fork();
-		
-		if (pid==0){
-			execvp(l->seq[0][0], l->seq[0]);
-			perror("execpv ");
+
+        if(pid==-1){perror("Fork ");exit(1);}
+
+		else if (pid==0){
+            int fdIn;
+            int fdOut;
+            if (l->in){
+                fdIn = Open(l->in, O_RDONLY, 0);
+                if (fdIn==-1){fprintf(stderr, "Problème à l'ouverture\n");exit(1);}
+                Close(0);
+                if(dup(fdIn) < 0){perror("dup2 ");}
+                Close(fdIn);
+            }
+            if (l->out){
+                fdOut = Open(l->out, O_CREAT | O_WRONLY, 0644);
+                if (fdOut==-1){fprintf(stderr, "Problème à l'ouverture\n");exit(1);}
+                Close(1);
+                if(dup(fdOut) < 0){perror("dup2 ");}
+                Close(fdOut);
+            }
+			if(execvp(l->seq[0][0], l->seq[0]) < 0){perror("execpv ");exit(1);}
+            Close(fdIn);
+            Close(fdOut);
 			exit(0);
 		}
 		Wait(NULL);
