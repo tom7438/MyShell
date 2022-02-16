970da3db6877601a66bf8dd2ea2953fdd4d33cd0
tom7438 <tom192010@live.fr>
2022-02-16 16:22:53 +0100

    Modif affichage shell

--- src/shell.c
+++ src/shell.c
@@ -50,10 +50,11 @@ int main(){
         printf("\033[%dm", couleur);
         printf("\033[4mMyShell:\033[00m");
         if(!strcmp(getenv("HOME"), getcwd(rep, 100))){
-            printf("\033[34m~>\033[00m ");
+            printf("\033[34m~\033[00m");
         } else{
-            printf("\033[34m%s>\033[00m ",getcwd(rep, 100));
+            printf("\033[34m%s\033[00m",getcwd(rep, 100));
         }
+        printf(" # ");
         if((couleur=(couleur+1-31)%18+31)==34){couleur++;};
 
         l = readcmd();
